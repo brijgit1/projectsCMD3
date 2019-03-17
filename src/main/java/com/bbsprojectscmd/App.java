cc46fb47728bb5b60d4431dca9a2d8d486b88c1b
B B SINGH1 <brijbhan.hbti@gmail.com>
2019-03-17 23:40:12 +0530

    First Commit

---
+++ src/main/java/com/bbsprojectscmd/App.java
@@ -0,0 +1,13 @@
+package com.bbsprojectscmd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
